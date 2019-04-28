107a7ef814ccdf321fc7fe9d51deb6caeb43756f
MostafaIn <mostafa.hazareh@integrify.fi>
2019-04-28 21:48:20 +0300

    Updates

---
+++ precache-manifest.a2181fa61fb4a67532fed942cc23d6b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd050673762c8cefba4372afcbf3f8cb",
+    "url": "/Student_Info-miniReactProject/index.html"
+  },
+  {
+    "revision": "72cab6570c605ef4d72a",
+    "url": "/Student_Info-miniReactProject/static/css/main.94ea19e8.chunk.css"
+  },
+  {
+    "revision": "72397b8db523684d3185",
+    "url": "/Student_Info-miniReactProject/static/js/2.e3845b44.chunk.js"
+  },
+  {
+    "revision": "72cab6570c605ef4d72a",
+    "url": "/Student_Info-miniReactProject/static/js/main.923715fb.chunk.js"
+  },
+  {
+    "revision": "f4630fba4ef71d44fa6b",
+    "url": "/Student_Info-miniReactProject/static/js/runtime~main.4fd02613.js"
+  }
+]);
